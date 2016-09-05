f8ef9fc694771fd225d2294ea11a7fea795e67c9
Edwin Rodriguez <Edwin Rodriguez>
2016-09-04 20:37:07 -0400

    sql create table

---
+++ add files.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO `bamazon`.`products` (`ID`, `Product_Name`, `Department_Name`, `Price`, `STOCK_QUANTITY`) VALUES ('1', 'Baseball Bat', 'Sporting', '39.99', '80');
+INSERT INTO `bamazon`.`products` (`ID`, `Product_Name`, `Department_Name`, `Price`, `STOCK_QUANTITY`) VALUES ('2', 'Baseball Glove', 'Sporting', '22.99', '92');
+INSERT INTO `bamazon`.`products` (`ID`, `Product_Name`, `Department_Name`, `Price`, `STOCK_QUANTITY`) VALUES ('3', 'Walking Dead DVD', 'Electronics', '19.99', '57');
+INSERT INTO `bamazon`.`products` (`ID`, `Product_Name`, `Department_Name`, `Price`, `STOCK_QUANTITY`) VALUES ('4', 'Mirror', 'Home Decor', '24.99', '30');
+INSERT INTO `bamazon`.`products` (`ID`, `Product_Name`, `Department_Name`, `Price`, `STOCK_QUANTITY`) VALUES ('5', 'Picture Frame', 'Home Decor', '15.99', '86');
+INSERT INTO `bamazon`.`products` (`ID`, `Product_Name`, `Department_Name`, `Price`, `STOCK_QUANTITY`) VALUES ('6', 'Jenga', 'Toys', '19.99', '124');
+INSERT INTO `bamazon`.`products` (`ID`, `Product_Name`, `Department_Name`, `Price`, `STOCK_QUANTITY`) VALUES ('7', 'GI Joe', 'Toys', '9.99', '92');
+INSERT INTO `bamazon`.`products` (`ID`, `Product_Name`, `Department_Name`, `Price`, `STOCK_QUANTITY`) VALUES ('8', 'Madden 17', 'Electronics', '59.99', '78');
+INSERT INTO `bamazon`.`products` (`ID`, `Product_Name`, `Department_Name`, `Price`, `STOCK_QUANTITY`) VALUES ('9', 'Nike Shorts', 'Sporting', '19.99', '99');
+INSERT INTO `bamazon`.`products` (`ID`, `Product_Name`, `Department_Name`, `Price`, `STOCK_QUANTITY`) VALUES ('10', 'Deep Fryer', 'Kitchen', '29.99', '40');
---
+++ create table.sql
@@ -0,0 +1,12 @@
+use Bamazon; 
+
+CREATE TABLE `products`(
+`ID` INT NOT NULL,
+`Product_Name` VARCHAR(100) NULL,
+`Department_Name` VARCHAR(100) NULL,
+`Price` DECIMAL (10,2) NULL,
+`STOCK_QUANTITY` INT (10),
+PRIMARY KEY (`ID`)
+);
+
+
